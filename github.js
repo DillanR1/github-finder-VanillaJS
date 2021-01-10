41f1a2cfb8b3f77fb7aac6364d3663e358330973
DillanR1 <dillanroby@yahoo.com>
2021-01-09 18:44:42 -0800

    App fetches and displays repo data correctly

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "2520b5b5db27aff36ea2";
     this.client_secret = "db7efc72751a374a081980085c5e893668d5e02a";
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-        profile
+        profile,
+        repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -31,20 +31,61 @@ class UI {
        `;
   }
 
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
   // Show alert message
   showAlert(message, className) {
-    // Create div 
-    const div = document.createElement('div');
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement("div");
     // Add classes
     div.className = className;
     // Add text
     div.appendChild(document.createTextNode(message));
     // Get parent
-    const container = document.querySelector('.searchContainer');
+    const container = document.querySelector(".searchContainer");
     // Get search box
-    const search = document.querySelector('.search');
+    const search = document.querySelector(".search");
     // Insert alert
     container.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
   }
 
   // Clear profile
